88218ea066527c97cb0b4d2342e3c4ad9bb63014
xR86 <9412441+xR86@users.noreply.github.com>
2017-12-23 23:56:39 +0200

    added chunking utils

--- kaggle/utils_pandas.py
+++ kaggle/utils_pandas.py
@@ -23,11 +23,11 @@ def feature_lst(lst=keep_this):
 def init():
 	limit_on_flag = False
 	if limit_on_flag:
-	    pd.set_option('display.max_rows', 60)
-	    pd.set_option('display.max_columns', 20)
+		pd.set_option('display.max_rows', 60)
+		pd.set_option('display.max_columns', 20)
 	else:
-	    pd.set_option('display.max_rows', None)
-	    pd.set_option('display.max_columns', None)
+		pd.set_option('display.max_rows', None)
+		pd.set_option('display.max_columns', None)
 
 	# default = 60
 	print('display.max_rows    = %s' % pd.get_option('display.max_rows'))
@@ -40,6 +40,33 @@ def init():
 def info(df):
 	print('Dataset range: %s rows, %s columns\n' % df.shape)
 	df.info(verbose=False)
+
+def chunker(df_tf, init_i = 0, i = 0, small = False):
+	# can be wrapped in tqdm ?
+	for chunk in df_tf:
+		if init_i != 0:
+			print('Step ' + str(i))
+			print('> left - ' + str(init_i) + ' skip')
+			init_i -= 1
+			i += 1
+			del chunk
+			continue
+		print('Step ' + str(i))
+		print('> Chunk accessed')
+		proc_small_df = chunk[lst]
+		print('> Chunk selected')
+
+		#if small:
+		#	pd.concat([new_proc_df, proc_small_df], ignore_index=True)
+
+		print('> Saving csv')
+		proc_small_df.to_csv('proc.csv.' + str(i))
+		del proc_small_df
+		del chunk
+		print('next')
+
+		i += 1
+
 
 if __name__ == '__main__':
 	print("List should be ['Feature 1', 'Feature 2']: %s" % feature_lst())
